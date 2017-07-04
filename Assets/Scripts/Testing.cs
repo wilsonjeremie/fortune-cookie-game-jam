fcd4b0666fafc2cd79486b3121ec3808d6e5374b
jrand35 <jrand35@aol.com>
2017-07-03 21:35:14 -0400

    Hi Guys again

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+    float rot;
+    float dRot = 128f;
+    Quaternion startRot;
+	void Start () {
+        startRot = transform.rotation;
+        rot = 0f;
+	}
+	
+	void Update () {
+        rot += Time.deltaTime * dRot;
+        transform.rotation = startRot * Quaternion.Euler(rot, rot, rot);
+	}
+}
