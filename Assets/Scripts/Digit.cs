2a9032a843e790e8be946159edb59afc9265adae
jrand35 <jrand35@aol.com>
2017-07-08 00:48:22 -0400

    Updated Distance Counter

---
+++ Assets/Scripts/Digit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Digit : MonoBehaviour {
+
+    public int Number
+    { 
+        set
+        {
+            digit = value;
+            SetActiveDigit(value);
+        }
+        get {return digit; }
+    }
+    public GameObject[] digits;
+    GameObject activeDigit;
+    int digit;
+
+    void Start()
+    {
+        InitializeDigits();
+    }
+
+    void InitializeDigits()
+    {
+        foreach (GameObject obj in digits)
+        {
+            obj.SetActive(false);
+        }
+        activeDigit = digits[0];
+        activeDigit.SetActive(true);
+    }
+
+    void SetActiveDigit(int index)
+    {
+        activeDigit.SetActive(false);
+        activeDigit = digits[index];
+        activeDigit.SetActive(true);
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+    public Digit[] scoreDigits;
     Player player;
     public Text distanceText;
     float distance;
@@ -14,12 +15,23 @@ public class GameController : MonoBehaviour {
 	}
 	
 	void Update () {
-        UpdateDistanceText();
+        UpdateDistance();
 	}
 
-    void UpdateDistanceText()
+    void UpdateDistance()
     {
         distance += player.Speed * Time.deltaTime;
-        distanceText.text = "Distance: " + ((int)distance).ToString();
+
+        int distInt = (int)distance;
+
+        scoreDigits[0].Number = (distInt / 100000000);
+        scoreDigits[1].Number = (distInt / 10000000) % 10;
+        scoreDigits[2].Number = (distInt / 1000000) % 10;
+        scoreDigits[3].Number = (distInt / 100000) % 10;
+        scoreDigits[4].Number = (distInt / 10000) % 10;
+        scoreDigits[5].Number = (distInt / 1000) % 10;
+        scoreDigits[6].Number = (distInt / 100) % 10;
+        scoreDigits[7].Number = (distInt / 10) % 10;
+        scoreDigits[8].Number = (distInt) % 10;
     }
 }
