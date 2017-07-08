310c8a15759791fc5ab6cbc596e006bfe7d115d7
jrand35 <jrand35@aol.com>
2017-07-08 00:06:59 -0400

    Added UI

---
+++ Assets/Scripts/EuphoriaDisplay.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EuphoriaDisplay : MonoBehaviour {
+
+    public GameObject[] smilies;
+    Player player;
+    GameObject activeSmiley;
+
+	void Start () {
+        player = FindObjectOfType<Player>();
+        InitializeSmileys();
+	}
+	
+	void Update () {
+        UpdateSmiley();
+	}
+
+    void InitializeSmileys()
+    {
+        foreach (GameObject smilie in smilies)
+        {
+            smilie.SetActive(false);
+        }
+        activeSmiley = smilies[0];
+    }
+
+    void UpdateSmiley()
+    {
+        float euphoria = player.Euphoria;
+
+        if (euphoria == 0f)
+        {
+            SetSmiley(0);
+        }
+        else if (euphoria > 0f && euphoria < 0.25f)
+        {
+            SetSmiley(1);
+        }
+        else if (euphoria >= 0.25f && euphoria < 0.5f)
+        {
+            SetSmiley(2);
+        }
+        else if (euphoria >= 0.5f && euphoria < 0.75f)
+        {
+            SetSmiley(3);
+        }
+        else if (euphoria >= 0.75f && euphoria < 1f)
+        {
+            SetSmiley(4);
+        }
+        else if (euphoria == 1f)
+        {
+            SetSmiley(5);
+        }
+    }
+
+    void SetSmiley(int index)
+    {
+        activeSmiley.SetActive(false);
+        activeSmiley = smilies[index];
+        activeSmiley.SetActive(true);
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
-
     Player player;
     public Text distanceText;
-    public Image euphoriaBar;
     float distance;
 
 	void Start () {
@@ -17,18 +15,11 @@ public class GameController : MonoBehaviour {
 	
 	void Update () {
         UpdateDistanceText();
-        UpdateEuphoriaBar();
 	}
 
     void UpdateDistanceText()
     {
         distance += player.Speed * Time.deltaTime;
         distanceText.text = "Distance: " + ((int)distance).ToString();
-    }
-
-    void UpdateEuphoriaBar()
-    {
-        float x = player.Euphoria;
-        euphoriaBar.transform.localScale = new Vector3(x, 1f, 1f);
     }
 }
